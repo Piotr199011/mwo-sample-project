285e9993e524bfbc4a3c9536b9ab0f56c81d98e0
jarmosz <piotr.jarmosz@o2.pl>
2025-05-10 12:10:33 +0200

    #1Error

--- mwo-sample-project/src/main/java/pl/edu/agh/mwo/Reader.java
+++ mwo-sample-project/src/main/java/pl/edu/agh/mwo/Reader.java
@@ -1,4 +1,30 @@
 package pl.edu.agh.mwo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reader {
+
+
+/*    URL url;
+
+    {
+        try {
+            url = new URL("https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159&speed>=315");
+            ObjectMapper mapper = new ObjectMapper();
+            List<Pit>pits = mapper.readValue(new File(url.toURI()));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    ArrayList<Pit> list = new ArrayList<>();
+for( Pit p:)*/
+
 }
