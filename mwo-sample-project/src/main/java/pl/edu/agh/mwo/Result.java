edf87ff74041f237a02dcc88fe3f6b40fcc86d2a
Andrij Kostecki <kostecki.andrij@gmail.com>
2025-05-10 12:13:04 +0200

    ResultPrinter and Result

---
+++ mwo-sample-project/src/main/java/pl/edu/agh/mwo/Result.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.mwo;
+
+public class Result {
+    private Calculator calculator;
+    public Result(Pit pit, Calculator calculator) {
+
+        this.calculator = calculator;
+    }
+
+
+}
---
+++ mwo-sample-project/src/main/java/pl/edu/agh/mwo/ResultPrinter.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.mwo;
+
+import java.io.PrintWriter;
+
+public class ResultPrinter {
+    private Result result;
+    public ResultPrinter() {
+
+    }
+
+    public void printPit() {}
+
+
+}
