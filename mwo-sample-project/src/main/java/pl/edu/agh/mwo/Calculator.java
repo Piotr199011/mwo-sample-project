56fa3b1ada815cb2a527ccbfa28b414957f41eff
laszczak <anita.laszczak@gmail.com>
2025-05-10 12:03:31 +0200

    cal v1

--- mwo-sample-project/src/main/java/pl/edu/agh/mwo/Calculator.java
+++ mwo-sample-project/src/main/java/pl/edu/agh/mwo/Calculator.java
@@ -1,4 +1,47 @@
 package pl.edu.agh.mwo;
 
+import java.util.ArrayList;
+
 public class Calculator {
+
+    private ArrayList<Pit> pitStop;
+
+    public Calculator(ArrayList<Pit> pitStop) {
+        this.pitStop = pitStop;
+    }
+
+
+    protected int getPitStopNumber() {
+        return pitStop.size();
+    }
+
+    protected Pit getPitStopWithLongestTime() {
+
+       double time = 0;
+        for (Pit pit : pitStop) {
+            double tempTime = pit.getPitDuration();
+            if(tempTime > time) {
+                return pit;
+            }
+        }
+
+        return null;
+    }
+
+    protected int getDriverIdByLongestPitStopTime() {
+        Pit newPit = getPitStopWithLongestTime();
+        return newPit.getDriverNumber();
+    }
+
+    protected double getPitStopAverageTime(){
+        double time = 0;
+        for (Pit pit : pitStop) {
+            time += pit.getPitDuration();
+        }
+        return time / pitStop.size();
+    }
+
+
 }
+
+
